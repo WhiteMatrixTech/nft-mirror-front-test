25d5d1c7ccb61920a1614bb774d45a1ec0456c6d
nemo <18136225579@163.com>
2021-09-18 18:35:10 +0800

    fix: build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite'
 import eslint from '@rollup/plugin-eslint'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import polyfillNode from 'rollup-plugin-polyfill-node'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     {...eslint({include: 'src/**/*.+(js|jsx|ts|tsx)'}), enforce: 'pre'},
-    reactRefresh(),
-    polyfillNode()
+    reactRefresh()
   ],
   define: {
     'process.env': {}
